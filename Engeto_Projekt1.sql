86e72f020980a01de69e42f05885cbb276330a21
Lubos Polak <lpolak@centrum.cz>
2023-04-10 16:58:32 +0200

    Updated SQL file for project 1

--- Engeto_Projekt1.sql
+++ Engeto_Projekt1.sql
@@ -7,7 +7,7 @@ WHERE payroll_year = 2000
 	AND calculation_code = 100
 ;
 
--- tvorba pomocné tabulky pro zjištění průměrných cen v jednotlivých letech
+-- tvorba pomocné tabulky pro zjištění průměrných platů v jednotlivých letech
 
 CREATE TABLE IF NOT EXISTS t_Lubos_Polak_project_SQL_wages (
 	id int,
@@ -102,18 +102,69 @@ DROP TABLE t_lubos_polak_project_sql_wages;
  * úkol 1: Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
  */
 
+SELECT
+	year,
+	industry_branch_code,
+	industry_branch_name,
+	average_wage,
+	average_wage - lag(average_wage)
+		OVER (PARTITION BY `industry_branch_code` ORDER BY `year`) AS difference
+FROM t_lubos_polak_project_sql_primary_final tlpf
+GROUP BY YEAR, industry_branch_code 
+ORDER BY industry_branch_code, year 
+;
 
 
 /*
  * úkol 2: Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd??
  */
 
+SELECT
+	year,
+	industry_branch_name,
+	round (average_wage/product_price, 1) AS litres_of_milk_for_average_wage
+FROM t_lubos_polak_project_sql_primary_final
+WHERE (YEAR = (SELECT min(`year`) 
+		FROM t_lubos_polak_project_sql_primary_final)
+	OR
+	YEAR = (SELECT max(`year`) 
+		FROM t_lubos_polak_project_sql_primary_final)
+		)
+	AND product_name LIKE 'Mléko%'
+ORDER BY industry_branch_name 
+;
 
+SELECT
+	year,
+	industry_branch_name,
+	round (average_wage/product_price, 1) AS kilograms_of_bread_for_average_wage
+FROM t_lubos_polak_project_sql_primary_final
+WHERE (
+	YEAR = (SELECT min(`year`) 
+		FROM t_lubos_polak_project_sql_primary_final)
+	OR
+	YEAR = (SELECT max(`year`) 
+		FROM t_lubos_polak_project_sql_primary_final)
+		)
+	AND product_name LIKE 'Chléb%' 
+ORDER BY industry_branch_name 
+;
 
 /*
  * úkol 3: Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
  */
 
+SELECT
+	product_name,
+	round(avg(product_price), 2) AS average_price,
+	round(max(product_price), 2) AS max_price,
+	concat (round((100 - avg(product_price)/max(product_price)*100), 1), '%') AS average_growth
+FROM t_lubos_polak_project_sql_primary_final tlpf
+GROUP BY product_name
+ORDER BY avg(product_price)/max(product_price) DESC
+;
+
+-- odpověď hledám na základě průměrné ceny produktu
 
 
 /*
@@ -121,11 +172,10 @@ DROP TABLE t_lubos_polak_project_sql_wages;
  */
 
 
+SELECT * FROM t_lubos_polak_project_sql_primary_final tlpf
 
 /*
  * úkol 5: Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, 
  * projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
  */
-
-
 
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Project 1 - SQL
 Přidávám samostatný SQL dotaz pro výzkumný úkol 1
 Přidávám tvorbu tabulky pro data mezd a cen potravin za ČR
+Přidávám dotazy úkol 1, úkol 2 a úkol 3
